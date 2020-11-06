bd2ec3f1693914b8aae67cd05c7f8956d24f4f4a
Muhammet Catal <mcatal55@hotmail.com>
2020-11-06 02:09:02 +0100

    Die folgende Bereichen wurde hinzugefügt. Exklusiv für Verkäufer, Veröffentlichte Profile, Weitere Services, Ratgeber & Tipps und Fußnote

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Immobilien-mit-Bootstrap
 Dieses Projekt wurde mit der Bootstrap-Bibliothek am Beispiel der Website https://www.immobilienscout24.de/ entwickelt. Es hat keinen kommerziellen Zweck.
+
+Die folgenden Ressourcen wurden ebenfalls verwendet:
+Icons von <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+Icons von <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+Icons von <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
--- sources/js/custom.js
+++ sources/js/custom.js
@@ -33,25 +33,27 @@ $(document).on('click', '.dropdown-menu', function (e) {
 
 //Inspiration start//
 
-$('#recipeCarousel').carousel({
-  interval: 10000
-})
 
-$('.carousel .carousel-item').each(function () {
-  var minPerSlide = 3;
-  var next = $(this).next();
-  if (!next.length) {
-      next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
+// $('#carousel-speed').carousel({
+//   interval: 10000
+// })
 
-  for (var i = 0; i < minPerSlide; i++) {
-      next = next.next();
-      if (!next.length) {
-          next = $(this).siblings(':first');
+$('.carousel .carousel-item').each(function(){
+    var minPerSlide = 4;
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+    for (var i=0;i<minPerSlide;i++) {
+        next=next.next();
+        if (!next.length) {
+        	next = $(this).siblings(':first');
+      	}
+        
+        next.children(':first-child').clone().appendTo($(this));
       }
-
-      next.children(':first-child').clone().appendTo($(this));
-  }
 });
+
 //Inspiration end//
